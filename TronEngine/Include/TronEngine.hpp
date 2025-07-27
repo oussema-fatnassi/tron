9011b16709db581dbd369970429ec6de3e434948
Oroitz Lago Ramos <oroitzlr@gmail.com>
2025-07-27 20:53:57 +0200

    chore: unnecesary break line

--- TronEngine/Include/TronEngine.hpp
+++ TronEngine/Include/TronEngine.hpp
@@ -16,7 +16,6 @@
 #define TRON_GAME_TARGET_DELTA (1.0 / TRON_GAME_TARGET_FPS)    // 8.33ms
 #define TRON_RENDER_TARGET_DELTA (1.0 / TRON_RENDER_TARGET_FPS) // 16.67ms
 
-
 // Utility macros
 // TODO: Check what is used for and if we need to keep it
 #define TRON_UNUSED(x) ((void)(x))
