8ab2e2df7c213cb30e7165d406c9d700ec7d2e6b
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2025-07-28 10:18:34 +0200

    feat: implement custom window procedure for handling close and destroy events

--- TronEngine/Source/Cpp/Core/WindowUtils.cpp
+++ TronEngine/Source/Cpp/Core/WindowUtils.cpp
@@ -1,11 +1,37 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
+#include "../../Include/EngineExports.hpp"
+
+LRESULT CALLBACK TronWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_CLOSE:
+        std::cout << "[Window] Close button clicked\n";
+        RequestEngineShutdown(); // Signal engine to stop
+        return 0;
+
+    case WM_DESTROY:
+        std::cout << "[Window] Window destroyed\n";
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        // Let D3D handle painting, just validate
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
+    default:
+        return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+    }
+}
 
 HWND CreateSimpleWindow(HINSTANCE hInstance, int width, int height, const std::wstring& title) {
     // 1. Define the window class
     WNDCLASSW wc = {};
-    wc.lpfnWndProc = DefWindowProcW;
+    wc.lpfnWndProc = TronWindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = L"TronEngineWindowClass";
     if (!RegisterClassW(&wc) && GetLastError() != ERROR_CLASS_ALREADY_EXISTS) {
