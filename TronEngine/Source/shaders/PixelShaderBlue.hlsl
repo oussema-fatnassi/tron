c2a628cbdb4857d3a1e0a8ee444d71c8591d06c8
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2025-07-30 09:43:39 +0200

    feat: add blue and rainbow pixel shaders for color rendering

---
+++ TronEngine/Source/shaders/PixelShaderBlue.hlsl
@@ -0,0 +1,9 @@
+cbuffer ColorBuffer : register(b1)
+{
+    float4 color;
+}
+
+float4 main(float4 pos : SV_POSITION, float3 color : COLOR) : SV_TARGET
+{
+    return float4(0.0, 0.0, 1.0, 1.0); // Bleu fixe
+}
